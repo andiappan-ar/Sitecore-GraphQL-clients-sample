b6d9cd5cffff05689ff0a2c6bc65f84c7147a3b3
Andiappan <ARandi@outlook.com>
2021-08-12 15:20:03 +0400

    Client console application included

---
+++ src/Sitecore.GraphQL.Client.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Sitecore.GraphQL.Authenticator.Authenticator;
+using Sitecore.GraphQL.Services.Service;
+using System;
+
+namespace Sitecore.GraphQL.ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string SSCURL = "https://arsc.dev.local/sitecore/api/ssc/auth/login";
+            string SCC_GRAPHURL = "https://arsc.dev.local/sitecore/api/graph/items/master";
+            string domain = "sitecore";
+            string siteDomain = "arsc.dev.local";
+
+            Console.WriteLine("***************** Login sitecore ********************"+Environment.NewLine);
+            Console.WriteLine("Enter user name:"+Environment.NewLine);
+            var userName = Console.ReadLine();
+            Console.WriteLine("Enter password:" + Environment.NewLine);
+            var password = Console.ReadLine();
+
+            var authCookie = Login.GetAuthroizedCookie(SSCURL, domain, userName, password);
+
+            var graphQLQuery = @"{
+                                      item {
+                                        name
+                                        children {
+                                          name                                            
+                                        }
+                                      }
+                                    }";
+
+            var graphQLData = ReadService.FetchGraphQLData<dynamic>(authCookie, SCC_GRAPHURL, siteDomain, graphQLQuery);
+
+            Console.WriteLine(JsonConvert.SerializeObject(graphQLData.Data));
+        }
+    }
+}
---
+++ src/Sitecore.GraphQL.Services/Service/ReadService.cs
@@ -0,0 +1,57 @@
+﻿using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sitecore.GraphQL.Services.Service
+{
+    public static class ReadService
+    {
+        public static async Task<GraphQLResponse<T>> GetGraphQLData<T>(string authCookie,string SCC_GRAPHURL,string siteDomain,string graphQLQuery)
+        {
+            GraphQLResponse<T> result;
+
+            try
+            {
+                CookieContainer cookieContainer = new CookieContainer();
+                cookieContainer.Add(new Cookie(".AspNet.Cookies", authCookie,"/", siteDomain));
+
+                var handler = new HttpClientHandler
+                {
+                    CookieContainer = cookieContainer,
+                    UseCookies = true
+                };
+
+                Uri scURI = new Uri(SCC_GRAPHURL);
+                using (var client = new GraphQLHttpClient(
+                new GraphQLHttpClientOptions { EndPoint = scURI, HttpMessageHandler = handler  }, new NewtonsoftJsonSerializer()
+                ))
+                {                  
+
+                    var request = new GraphQLRequest(graphQLQuery);
+                    result = await client.SendQueryAsync<T>(request);
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return result;
+        }
+
+        public static GraphQLResponse<T> FetchGraphQLData<T>(string authCookie, string SCC_GRAPHURL, string siteDomain, string graphQLQuery)
+        {
+            var result = Task.Run(async () => await GetGraphQLData<T>(authCookie, SCC_GRAPHURL, siteDomain, graphQLQuery)).Result;
+            return result;
+        }
+
+    }
+}
+
